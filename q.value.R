6c5c1c7fb5296d9f3180d3be4bd125e970acdd95
jgould <jgould@47fccdf2-e235-0410-87e5-63258e95d6e5>
2004-09-24 23:12:39 +0000

    computes fdr
    git-svn-id: https://svn.broadinstitute.org/gp2/trunk/modules/ComparativeMarkerSelection@3719 47fccdf2-e235-0410-87e5-63258e95d6e5

---
+++ q.value.R
@@ -0,0 +1,29 @@
+compute.q.values <- function(p.value.file='p.values.txt', indices.file='indices.txt', previous.task.values, output.file.name='test.txt') {
+	#on.exit(unlink(c(p.value.file, indices.file, previous.task.values)))
+	library(qvalue)
+	
+	p.values <- read.table(p.value.file)[1,]
+	p.values.vector <- vector()
+	for(i in 1:length(p.values)) {
+		p.values.vector[i] <- as.numeric(p.values[[i]])
+	}
+	print(p.values.vector)
+	result <- qvalue(p.values.vector)
+	qvalues <- result$qvalues
+	
+	previous.values <- read.table(previous.task.values, sep="\t")
+	m <- matrix(nrow=NROW(previous.values), ncol=NCOL(previous.values)+1)
+	for(i in 1:NCOL(previous.values)) {
+		m[, i] <- previous.values[,i]
+	}
+
+	indices <- read.table(indices.file)[1,]
+	last.column <- NCOL(previous.values)+1
+	for(i in 1:NROW(previous.values)) {
+		m[i, last.column] <- qvalues[indices[[i]]+1]
+	}
+
+	write.table(m, output.file.name, sep="\t", col.names=FALSE, quote=FALSE)
+	
+	return(output.file.name)
+}

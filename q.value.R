c23f59cfe818bb77834f26fb8562e344ff86ebb5
jgould <jgould@47fccdf2-e235-0410-87e5-63258e95d6e5>
2004-09-27 22:53:02 +0000

    cleaned up code
    git-svn-id: https://svn.broadinstitute.org/gp2/trunk/modules/ComparativeMarkerSelection@3736 47fccdf2-e235-0410-87e5-63258e95d6e5

--- q.value.R
+++ q.value.R
@@ -1,27 +1,9 @@
-compute.q.values <- function(p.value.file='p.values.txt', indices.file='indices.txt', previous.task.values, output.file.name='r_output.txt') {
-	#on.exit(unlink(c(p.value.file, indices.file, previous.task.values)))
+compute.q.values <- function(previous.task.values, output.file.name='r_output.txt') {
 	library(qvalue)
-	
-	p.values <-scan(p.value.file)
-	result <- qvalue(p.values)
+	values <- read.table(previous.task.values, sep="\t", row.names=1)
+	result <- qvalue(values[,3])
 	qvalues <- result$qvalues
-	
-	previous.values <- read.table(previous.task.values, sep="\t", row.names=1)
-	
-	m <- matrix(nrow=NROW(previous.values), ncol=NCOL(previous.values)+1)
-	
-	row.names(m) <- row.names(previous.values)
-	for(i in 1:NCOL(previous.values)) {
-		m[, i] <- previous.values[, i]
-	}
-
-	indices <- scan(indices.file)
-	last.column <- NCOL(previous.values)+1
-	for(i in 1:NROW(previous.values)) {
-		m[i, last.column] <- qvalues[indices[[i]]+1]
-	}
-
-	write.table(m, output.file.name, sep="\t", col.names=FALSE, quote=FALSE)
-	
+	values <- cbind(values, qvalues)
+	write.table(values, output.file.name, sep="\t", col.names=FALSE, quote=FALSE)
 	return(output.file.name)
 }

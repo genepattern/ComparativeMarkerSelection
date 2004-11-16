1027c2c59ed4278de3044aa7c3a3b8628fc311a5
jgould <jgould@47fccdf2-e235-0410-87e5-63258e95d6e5>
2004-11-16 23:56:57 +0000

    no longer used
    git-svn-id: https://svn.broadinstitute.org/gp2/trunk/modules/ComparativeMarkerSelection@4447 47fccdf2-e235-0410-87e5-63258e95d6e5

--- q.value.R
+++
@@ -1,9 +0,0 @@
-compute.q.values <- function(previous.task.values, output.file.name='r_output.txt') {
-	library(qvalue)
-	values <- read.table(previous.task.values, sep="\t", row.names=1)
-	result <- qvalue(values[,3])
-	qvalues <- result$qvalues
-	values <- cbind(values, qvalues)
-	write.table(values, output.file.name, sep="\t", col.names=FALSE, quote=FALSE)
-	return(output.file.name)
-}

913dc054f09e97bbd70a9c1ab1800ebce0e66d55
jgould <jgould@47fccdf2-e235-0410-87e5-63258e95d6e5>
2005-08-09 02:06:33 +0000

    Added additional field
    git-svn-id: https://svn.broadinstitute.org/gp2/trunk/modules/ComparativeMarkerSelection@6836 47fccdf2-e235-0410-87e5-63258e95d6e5

--- src/edu/mit/broad/marker/Constants.java
+++ src/edu/mit/broad/marker/Constants.java
@@ -3,25 +3,31 @@ package edu.mit.broad.marker;
 public class Constants {
 	
 	private Constants(){}
-	
+
+	/** Test direction */	
 	final static int CLASS_ZERO_GREATER_THAN_CLASS_ONE = 0;
 
 	final static int CLASS_ZERO_LESS_THAN_CLASS_ONE = 1;
 
 	final static int TWO_SIDED = 2;
+	
+	/** Test statistic */
 
 	final static int T_TEST = 0;
+	
+	final static int T_TEST_MEDIAN = 2;
+	
+	final static int T_TEST_MIN_STD = 4;
+
+	final static int T_TEST_MEDIAN_MIN_STD = 7;
+	
 
 	final static int SNR = 1;
 
-	final static int T_TEST_MEDIAN = 2;
-
 	final static int SNR_MEDIAN = 3;
-
-	final static int T_TEST_MIN_STD = 4;
-
+	
 	final static int SNR_MIN_STD = 5;
 
 	final static int SNR_MEDIAN_MIN_STD = 6;
-
+	
 }

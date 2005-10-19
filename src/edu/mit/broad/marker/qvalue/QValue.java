a7deb0744bf52b373e15da44179a2e8abdde20cc
jgould <jgould@47fccdf2-e235-0410-87e5-63258e95d6e5>
2005-10-19 16:58:19 +0000

    Added parameter to print stderr and stdout
    git-svn-id: https://svn.broadinstitute.org/gp2/trunk/modules/ComparativeMarkerSelection@7319 47fccdf2-e235-0410-87e5-63258e95d6e5

--- src/edu/mit/broad/marker/qvalue/QValue.java
+++ src/edu/mit/broad/marker/qvalue/QValue.java
@@ -1,22 +1,25 @@
 package edu.mit.broad.marker.qvalue;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 /**
- *@author    Joshua Gould
+ * @author Joshua Gould
  */
 public class QValue {
 	private final static String R_FILE = "qvalue.R";
 
-
-	public static void qvalue(String markerInputFile) {
+	public static void qvalue(String markerInputFile, boolean printRErrors) {
 		try {
-			String[] args = {"'" + markerInputFile + "'"};
-			edu.mit.broad.marker.r.R.run(new File(System.getProperty("libdir"), R_FILE).getCanonicalPath(), "compute.qvalue", args, System.getProperty("R"), false, false);
-		} catch(Exception ioe) {
+			String[] args = { "'" + markerInputFile + "'" };
+			edu.mit.broad.marker.r.R.run(new File(System.getProperty("libdir"),
+					R_FILE).getCanonicalPath(), "compute.qvalue", args, System
+					.getProperty("R"), printRErrors, printRErrors);
+		} catch (Exception ioe) {
 			System.err.println("Error computing q value");
-		} 
+		}
 	}
 }

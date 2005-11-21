a8f8a1cbc0cbd2e4b68fa81eaf270909005d9015
jgould <jgould@47fccdf2-e235-0410-87e5-63258e95d6e5>
2005-11-21 16:55:20 +0000

    Added return value
    git-svn-id: https://svn.broadinstitute.org/gp2/trunk/modules/ComparativeMarkerSelection@7635 47fccdf2-e235-0410-87e5-63258e95d6e5

--- src/edu/mit/broad/marker/qvalue/QValue.java
+++ src/edu/mit/broad/marker/qvalue/QValue.java
@@ -12,14 +12,17 @@ import java.io.IOException;
 public class QValue {
 	private final static String R_FILE = "qvalue.R";
 
-	public static void qvalue(String markerInputFile, boolean printRErrors) {
+	public static boolean qvalue(String markerInputFile, boolean printRErrors) {
 		try {
 			String[] args = { "'" + markerInputFile + "'" };
 			edu.mit.broad.marker.r.R.run(new File(System.getProperty("libdir"),
 					R_FILE).getCanonicalPath(), "compute.qvalue", args, System
 					.getProperty("R"), printRErrors, printRErrors);
+			return true;
 		} catch (Exception ioe) {
-			System.err.println("Error computing q value");
+			System.err
+					.println("An error occurred while computing q value-continuing anyway");
+			return false;
 		}
 	}
 }
